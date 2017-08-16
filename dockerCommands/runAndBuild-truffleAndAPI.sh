a8db79e9403e9a50a503d9e466464cc28b13062b
mgsgde <mgsgde@gmail.com>
2017-08-16 12:30:02 +0200

    added cloud9

--- dockerCommands/runAndBuild-truffleAndAPI.sh
+++ dockerCommands/runAndBuild-truffleAndAPI.sh
@@ -5,4 +5,4 @@ pathSwagger="$(pwd)/swagger"
 
 # Delete all containers
 sudo docker rm -f truffleAndAPIName
-sudo docker run --link mysqlName:mysql --link testrpcName:testrpc --name truffleAndAPIName -v $pathTruffle:/src/truffle/ -v $pathSwagger:/src/swagger -it -p 8081:8081 -p 2222:2222 -p 8080:8080 truffleandapi
+sudo docker run --link mysqlName:mysql --link testrpcName:testrpc --name truffleAndAPIName -v $pathTruffle:/src/truffle/ -v $pathSwagger:/src/swagger -it -p 8181:8181 -p 8081:8081 -p 2222:2222 -p 8080:8080 truffleandapi
