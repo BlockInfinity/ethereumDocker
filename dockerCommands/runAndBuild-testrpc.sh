cf5187d673f87aec17c7dd3357c244b338dc2ea1
mgsgde <mgsgde@gmail.com>
2017-08-16 12:01:29 +0200

    added ethereum node container

--- dockerCommands/runAndBuild-testrpc.sh
+++ dockerCommands/runAndBuild-testrpc.sh
@@ -1,4 +1,6 @@
 sudo docker build -t testrpc -f Dockerfile.testrpc .
 
 sudo docker rm -f testrpcName
+sudo docker rm -f ethereumName
+
 sudo docker run --detach -it -p 8545:8545 --name testrpcName testrpc
